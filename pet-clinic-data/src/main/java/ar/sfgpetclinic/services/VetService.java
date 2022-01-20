0c407980c10ea963b9bc683dbbb5ae64abc3b998
Jorge Cardoso <jdcardos86@gmail.com>
2022-01-20 21:36:41 +0000

    SFGPETCLINIC: Adds a new Common Service Closes #11

--- pet-clinic-data/src/main/java/ar/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/ar/sfgpetclinic/services/VetService.java
@@ -5,12 +5,6 @@ import ar.sfgpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-    Vet findByLastName(String LastName);
+public interface VetService extends CrudService<Vet, Long>{
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
