cb05702fe041bad784c5c4ddc132c9708dc60d52
Jorge Cardoso <jdcardos86@gmail.com>
2022-03-15 22:19:27 +0000

    SFGPETCLINIC: Added a PetType to BootStrap load process

--- pet-clinic-data/src/main/java/ar/sfgpetclinic/services/map/PetTypeMapService.java
+++ pet-clinic-data/src/main/java/ar/sfgpetclinic/services/map/PetTypeMapService.java
@@ -2,10 +2,11 @@ package ar.sfgpetclinic.services.map;
 
 import ar.sfgpetclinic.model.PetType;
 import ar.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
-
+@Service
 public class PetTypeMapService extends AbstractMapService <PetType, Long> implements PetTypeService {
 
     @Override
--- pet-clinic-web/src/main/java/ar/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/ar/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,11 +1,11 @@
 package ar.sfgpetclinic.bootstrap;
 
 import ar.sfgpetclinic.model.Owner;
+import ar.sfgpetclinic.model.PetType;
 import ar.sfgpetclinic.model.Vet;
 import ar.sfgpetclinic.services.OwnerService;
+import ar.sfgpetclinic.services.PetTypeService;
 import ar.sfgpetclinic.services.VetService;
-import ar.sfgpetclinic.services.map.OwnerServiceMap;
-import ar.sfgpetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -14,14 +14,28 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+
+
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
